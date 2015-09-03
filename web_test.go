3173a8619fdd87d0f91a9a3e8cf2205b251a76a9
Stephen Cornelius <stephencornelius@brians-air.home>
2015-09-03 21:22:55 +0100

    still can't write a test

--- web_test.go
+++ web_test.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	_ "net/http"
-	_ "net/http/httptest"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-/* func TestSayHello(test *testing.T) {
+func TestSayHello(test *testing.T) {
 
 	//it return Hello World on the homepage
+	router := gin.New()
 	request, _ := http.NewRequest("GET", "/", nil)
-	response := httptest.NewRecorder()
-	assert.Equal(test, response.Body.String(), "Fuck You")
+	writer := httptest.NewRecorder()
+	router.ServeHTTP(writer, request)
+	assert.Equal(test, "Fuck You", writer.Body.String())
 
-} */
+}
 
 func TestWhatever(test *testing.T) {
 	x := Whatever()
-	assert.Equal(test, x, "bob")
+	assert.Equal(test, "bob", x)
 }
